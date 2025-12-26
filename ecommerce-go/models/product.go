5f6c0d137ad325f1bf15332ebb83e681dce4949d
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-12-26 03:38:42 +0100

    first commit

--- ecommerce-go/models/product.go
+++ ecommerce-go/models/product.go
@@ -1,8 +1,7 @@
 package models
 
-type User struct {
-	ID       uint   `gorm:"primaryKey" json:"id"`
-	Name     string `json:"name"`
-	Email    string `gorm:"unique" json:"email"`
-	Password string `json:"-"`
+type Product struct {
+	ID    uint    `gorm:"primaryKey" json:"id"`
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
 }
