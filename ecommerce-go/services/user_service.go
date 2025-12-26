67ce43e8e269882d31ed1215173937304e551b5b
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-12-26 03:40:01 +0100

    first commit

---
+++ ecommerce-go/services/user_service.go
@@ -0,0 +1,26 @@
+package services
+
+import (
+	"errors"
+
+	"ecommerce-go/models"
+	"ecommerce-go/repositories"
+)
+
+func RegisterUser(name, email, password string) (*models.User, error) {
+	user := models.User{
+		Name:     name,
+		Email:    email,
+		Password: password, // (later we hash it)
+	}
+	err := repositories.CreateUser(&user)
+	return &user, err
+}
+
+func LoginUser(email, password string) (*models.User, error) {
+	user, err := repositories.GetUserByEmail(email)
+	if err != nil || user.Password != password {
+		return nil, errors.New("invalid email or password")
+	}
+	return user, nil
+}
