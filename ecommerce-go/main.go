693b6b9cfcb98b3e3b861b28725f13a99c0fa39d
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-12-26 03:28:06 +0100

    first commit

--- ecommerce-go/main.go
+++ ecommerce-go/main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+
+	"ecommerce-go/config"
+	"ecommerce-go/models"
 	"ecommerce-go/routes"
 )
 
 func main() {
 	r := gin.Default()
+
+	config.ConnectDatabase()
+	config.DB.AutoMigrate(&models.Product{})
 
 	routes.RegisterRoutes(r)
 
