ecdd4e121c5ea232b529892b9685e055a8fe4fae
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-12-26 04:04:17 +0100

    first commit

---
+++ ecommerce-go/controllers/order_controller.go
@@ -0,0 +1,30 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"ecommerce-go/services"
+)
+
+type CheckoutInput struct {
+	Total float64 `json:"total"`
+}
+
+func Checkout(c *gin.Context) {
+	userID := c.GetUint("user_id")
+
+	var input CheckoutInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	order, err := services.Checkout(userID, input.Total)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	c.JSON(http.StatusCreated, order)
+}
