cf99f11205ab5bbae77bc9c8368c7a21f2074404
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-12-26 04:00:17 +0100

    first commit

--- ecommerce-go/routes/routes.go
+++ ecommerce-go/routes/routes.go
@@ -2,15 +2,20 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+
 	"ecommerce-go/controllers"
+	"ecommerce-go/middleware"
 )
 
 func RegisterRoutes(r *gin.Engine) {
 	r.GET("/products", controllers.GetProducts)
 	r.POST("/products", controllers.CreateProduct)
 	r.POST("/register", controllers.Register)
-    r.POST("/login", controllers.Login)
-    r.POST("/cart", controllers.AddToCart)
+	r.POST("/login", controllers.Login)
 
-
+	auth := r.Group("/")
+	auth.Use(middleware.AuthMiddleware())
+	{
+		auth.POST("/cart", controllers.AddToCart)
+	}
 }
