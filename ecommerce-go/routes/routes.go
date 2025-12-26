3c25f790eea0c993b1557bcecc2462bb6f5f3647
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-12-26 03:42:11 +0100

    first commit

--- ecommerce-go/routes/routes.go
+++ ecommerce-go/routes/routes.go
@@ -8,4 +8,7 @@ import (
 func RegisterRoutes(r *gin.Engine) {
 	r.GET("/products", controllers.GetProducts)
 	r.POST("/products", controllers.CreateProduct)
+	r.POST("/register", controllers.Register)
+    r.POST("/login", controllers.Login)
+
 }
