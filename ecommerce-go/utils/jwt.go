c193c0f5b788c288c6891920889d3aaf9f46b248
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-12-26 04:19:28 +0100

    first commit

--- ecommerce-go/utils/jwt.go
+++ ecommerce-go/utils/jwt.go
@@ -8,12 +8,13 @@ import (
 
 var jwtSecret = []byte("secret-key") // change later
 
-func GenerateToken(userID uint) (string, error) {
+func GenerateToken(userID uint, role string) (string, error) {
 	claims := jwt.MapClaims{
 		"user_id": userID,
+		"role":    role,
 		"exp":     time.Now().Add(time.Hour * 24).Unix(),
 	}
-
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString(jwtSecret)
 }
+
