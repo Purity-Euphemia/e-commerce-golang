be1ebf2dab1ff64d7ce82551514198ed7344a363
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-12-26 03:35:01 +0100

    first commit

---
+++ ecommerce-go/repositories/user_repository.go
@@ -0,0 +1,16 @@
+package repositories
+
+import (
+	"ecommerce-go/config"
+	"ecommerce-go/models"
+)
+
+func CreateUser(user *models.User) error {
+	return config.DB.Create(user).Error
+}
+
+func GetUserByEmail(email string) (*models.User, error) {
+	var user models.User
+	err := config.DB.Where("email = ?", email).First(&user).Error
+	return &user, err
+}
