9ca421fa0721049baed661dad2e26e49a302361b
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-12-27 21:53:58 +0100

    first commit

--- ecommerce-go/repositories/order_repository.go
+++ ecommerce-go/repositories/order_repository.go
@@ -8,3 +8,9 @@ import (
 func CreateOrder(order *models.Order) error {
 	return config.DB.Create(order).Error
 }
+
+func GetOrdersByUser(userID uint) ([]models.Order, error) {
+	var orders []models.Order
+	err := config.DB.Where("user_id = ?", userID).Find(&orders).Error
+	return orders, err
+}
