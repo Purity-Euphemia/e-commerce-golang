82cac25ccd72d429d66866bc8189209d19ceb52e
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-12-27 21:58:31 +0100

    first commit

--- ecommerce-go/services/product_services.go
+++ ecommerce-go/services/product_services.go
@@ -17,3 +17,16 @@ func CreateProduct(name string, price float64) (*models.Product, error) {
 	err := repositories.CreateProduct(&product)
 	return &product, err
 }
+func ReduceStock(productID, qty int) error {
+	for i, p := range products {
+		if p.ID == productID {
+			if p.Stock < qty {
+				return errors.New("not enough stock")
+			}
+			products[i].Stock -= qty
+			return nil
+		}
+	}
+	return errors.New("product not found")
+}
+
