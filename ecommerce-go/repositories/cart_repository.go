871127dc4ec1e1b7252bd3ea4ce5d8280a591cf8
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-12-27 22:02:53 +0100

    first commit

--- ecommerce-go/repositories/cart_repository.go
+++ ecommerce-go/repositories/cart_repository.go
@@ -8,3 +8,9 @@ import (
 func AddToCart(cart *models.Cart) error {
 	return config.DB.Create(cart).Error
 }
+func GetCartItemsByUser(userID uint) ([]models.Cart, error) {
+	var items []models.Cart
+	err := config.DB.Where("user_id = ?", userID).Find(&items).Error
+	return items, err
+}
+
