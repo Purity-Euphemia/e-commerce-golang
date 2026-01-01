65dbd52bd46076f319a91ea074ce03320c09c609
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-01-01 19:13:53 +0100

    first commit

--- ecommerce-go/repositories/cart_repository.go
+++ ecommerce-go/repositories/cart_repository.go
@@ -13,4 +13,17 @@ func GetCartItemsByUser(userID uint) ([]models.Cart, error) {
 	err := config.DB.Where("user_id = ?", userID).Find(&items).Error
 	return items, err
 }
+func UpdateCartItem(userID, productID uint, qty int) error {
+	return config.DB.
+		Model(&models.Cart{}).
+		Where("user_id = ? AND product_id = ?", userID, productID).
+		Update("quantity", qty).Error
+}
+
+func DeleteCartItem(userID, productID uint) error {
+	return config.DB.
+		Where("user_id = ? AND product_id = ?", userID, productID).
+		Delete(&models.Cart{}).Error
+}
+
 
