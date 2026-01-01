141c9a285400fe636ebca40364eac137a797baba
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-01-01 19:14:41 +0100

    first commit

--- ecommerce-go/services/cart_service.go
+++ ecommerce-go/services/cart_service.go
@@ -11,3 +11,14 @@ func AddProductToCart(userID, productID uint, qty int) error {
 	}
 	return repositories.AddToCart(&cart)
 }
+func UpdateCart(userID, productID uint, qty int) error {
+	if qty <= 0 {
+		return errors.New("quantity must be greater than zero")
+	}
+	return repositories.UpdateCartItem(userID, productID, qty)
+}
+
+func RemoveFromCart(userID, productID uint) error {
+	return repositories.DeleteCartItem(userID, productID)
+}
+
