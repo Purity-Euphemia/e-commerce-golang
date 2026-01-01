7d808719d66375dcef9404638a04d84d831e09fc
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-01-01 19:17:36 +0100

    first commit

--- ecommerce-go/controllers/cart_controller.go
+++ ecommerce-go/controllers/cart_controller.go
@@ -28,3 +28,35 @@ func AddToCart(c *gin.Context) {
 
 	c.JSON(http.StatusCreated, gin.H{"message": "Product added to cart"})
 }
+func UpdateCartItem(c *gin.Context) {
+	userID := c.GetUint("user_id")
+
+	var input UpdateCartInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	err := services.UpdateCart(userID, input.ProductID, input.Quantity)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "cart updated"})
+}
+
+func DeleteCartItem(c *gin.Context) {
+	userID := c.GetUint("user_id")
+	productIDParam := c.Param("product_id")
+
+	productID64, _ := strconv.ParseUint(productIDParam, 10, 64)
+
+	err := services.RemoveFromCart(userID, uint(productID64))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "item removed"})
+}
